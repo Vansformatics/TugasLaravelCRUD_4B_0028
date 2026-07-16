3df65bbd34f4aab3f607d34493cd9f10fd979351
Vansformatics <ivansayfuddin8@gmail.com>
2026-07-16 14:02:19 +0700

    membuat beberapa rute untuk public, guest, admin, crud dan logout

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
-// Rute Landing Page
+// Public Routes
+// Landing Page 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 
-// Rute CRUD untuk Post
-Route::resource('post', PostController::class)->except(['index']);
+// Guest
+Route::middleware(['guest'])->group(function () {
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Protected Routes
+Route::middleware(['auth'])->group(function () {
+    // CRUD
+    Route::resource('post', PostController::class)->except(['index', 'show']);
+});
+
+// Membaca Detail Isi Berita
+Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
